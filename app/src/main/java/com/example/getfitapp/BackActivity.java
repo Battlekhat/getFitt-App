f66988e7c6a282119e5dfc20b92de5445b7a6687
Les <lsalmeron@onesteppower.com>
2022-08-06 18:38:09 -0500

    Fixed errors and warnings related to constraints on MainActivity. Need BackActivity to show when clicked on MainActivity screen.

--- app/src/main/java/com/example/getfitapp/BackActivity.java
+++ app/src/main/java/com/example/getfitapp/BackActivity.java
@@ -2,13 +2,42 @@ package com.example.getfitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class BackActivity extends AppCompatActivity {
 
+    Button returnToMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back);
+
+        returnToMain = (Button) findViewById(R.id.button);
+        returnToMain.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+        VideoView vidView= (VideoView)findViewById(R.id.bentOverRow);
+
+        MediaController mediaControl = new MediaController(this);
+        mediaControl.setAnchorView(vidView);
+        Uri uri=Uri.parse("<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/pyBHPoC8Nzo\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>");
+        vidView.setMediaController(mediaControl);
+        vidView.setVideoURI(uri);
+        vidView.requestFocus();
+        vidView.start();
     }
 }
--- app/src/main/java/com/example/getfitapp/MainActivity.java
+++ app/src/main/java/com/example/getfitapp/MainActivity.java
@@ -2,14 +2,31 @@ package com.example.getfitapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        backButton = (ImageButton) findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(getApplicationContext(), BackActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
